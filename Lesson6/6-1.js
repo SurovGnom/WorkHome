b513cb517f3ea05bdc29531a3870e42ce2e6a1fe
SurovGnom <eremeevroma3@gmail.com>
2024-12-01 15:57:06 +0200

    redux 6-1 homework

--- Lesson6/6-1.js
+++ Lesson6/6-1.js
@@ -4,10 +4,36 @@ const chars = prompt('Enter some chars for removing');
 
 // function foo(string: string, arr: Array<string>): string
 function foo(string, arr) {
-    let array = Array.from(string);
-    array.splice(arr.length - 1, arr.length);
+   let array = "";
+   let i = 0;
+
+   if(!string){
+    return null;
+   }
+  while (i < string.length) {
+    let mate = true;
+
+    for (let g = 0; g < arr.length; g++) {
+      if (string[i + g] !== arr[g]) {
+         mate = false;
+        break;
+      }
+    }
+
+    if (!mate) {
+      array += string[i];
+      i++;
+      continue;
+    }
+
+    i += arr.length;
+  }
+
+
     return array;
+
 }
+
 
 const result = foo(message, chars);
 
