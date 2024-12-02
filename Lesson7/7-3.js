66179ae23f83ea1fb99461722249b2d3c6cfee2f
SurovGnom <eremeevroma3@gmail.com>
2024-12-02 20:55:21 +0200

    redux 7-3 homework

--- Lesson7/7-3.js
+++ Lesson7/7-3.js
@@ -1,20 +1,27 @@
 function printChoise() {
-    let input;
     let maxIterations = 10;
+    let result = '';
     for (let i = 0; i < maxIterations; i++) {
-        input = prompt("Введіть число");
-     if(!input){
-         console.log("Canceled");
-         break;
-     }else if(+input < 100){
-        console.log("Less than 100 :" ,input);
-     }else if (+input > 100 ){
-         console.log("More than 100 :" ,input);
-         break;
-     }else if (!+input){
-         console.log("This is text:" ,input);
-         break;
-     }
+        let input= prompt("Enter the number");
+        if(input <100) {
+            result = confirm(`You are sure :${input}`);
+
+       if(!result){
+        console.log(input)
+        return false;
+    }
+    }
+    if(input >=100 || isNaN(input)) {
+        console.log(`Last user\`s value: ${input}`);
+        break;
+    }
+
     }
 }
+
+
+
+
+    console.log(result);
+
 printChoise()
